ca1ecb503c1c8418fe83f0167e09dd70d3d11f74
M-Manoj-official <manojm1705@outlook.com>
2025-08-20 11:11:08 +0530

    Added footer

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+const footer = "2025 © E-commerce. All rights reserved.";
+import "./footer.css";
+function Footer() {
+  return (
+    <footer>
+      <p>{footer}</p>
+    </footer>
+  );
+}
+
+export default Footer;
